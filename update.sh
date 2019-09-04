50b17527d8529790ba8b8e4e4195c3562b21d436
Ichinose Shogo <shogo82148@gmail.com>
2019-09-04 23:12:53 +0900

    add PHP 7.1

--- update.sh
+++ update.sh
@@ -2,6 +2,10 @@
 
 (
     cd ubuntu/1.0 && \
+    echo updating php7.1 node10 standard-1.0 >&2 && \
+    ./update.pl 7.1 10 && \
+    echo updating php7.1 node12 standard-1.0 >&2 && \
+    ./update.pl 7.1 12 && \
     echo updating php7.2 node10 standard-1.0 >&2 && \
     ./update.pl 7.2 10 && \
     echo updating php7.2 node12 standard-1.0 >&2 && \
@@ -14,6 +18,10 @@
 
 (
     cd ubuntu/2.0 && \
+    echo updating php7.1 node10 standard-2.0 >&2 && \
+    ./update.pl 7.1 10 && \
+    echo updating php7.1 node12 standard-2.0 >&2 && \
+    ./update.pl 7.1 12 && \
     echo updating php7.2 node10 standard-2.0 >&2 && \
     ./update.pl 7.2 10 && \
     echo updating php7.2 node12 standard-2.0 >&2 && \
@@ -26,6 +34,10 @@
 
 (
     cd al2/1.0 && \
+    echo updating php7.1 node10 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.1 10 && \
+    echo updating php7.1 node12 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.1 12 && \
     echo updating php7.2 node10 amazonlinux2-1.0 >&2 && \
     ./update.pl 7.2 10 && \
     echo updating php7.2 node12 amazonlinux2-1.0 >&2 && \
