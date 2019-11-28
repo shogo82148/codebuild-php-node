2ffbc2549a3117ed2b2d625997c789bc6140670a
Ichinose Shogo <shogo82148@gmail.com>
2019-11-28 10:54:29 +0900

    add Amazon Linux 2 2.0 and Standard 3.0 image

--- update.sh
+++ update.sh
@@ -33,7 +33,39 @@
 )
 
 (
+    cd ubuntu/3.0 && \
+    echo updating php7.1 node10 standard-2.0 >&2 && \
+    ./update.pl 7.1 10 && \
+    echo updating php7.1 node12 standard-2.0 >&2 && \
+    ./update.pl 7.1 12 && \
+    echo updating php7.2 node10 standard-2.0 >&2 && \
+    ./update.pl 7.2 10 && \
+    echo updating php7.2 node12 standard-2.0 >&2 && \
+    ./update.pl 7.2 12 && \
+    echo updating php7.3 node10 standard-2.0 >&2 && \
+    ./update.pl 7.3 10 && \
+    echo updating php7.3 node12 standard-2.0 >&2 && \
+    ./update.pl 7.3 12
+)
+
+(
     cd al2/1.0 && \
+    echo updating php7.1 node10 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.1 10 && \
+    echo updating php7.1 node12 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.1 12 && \
+    echo updating php7.2 node10 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.2 10 && \
+    echo updating php7.2 node12 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.2 12 && \
+    echo updating php7.3 node10 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.3 10 && \
+    echo updating php7.3 node12 amazonlinux2-1.0 >&2 && \
+    ./update.pl 7.3 12
+)
+
+(
+    cd al2/2.0 && \
     echo updating php7.1 node10 amazonlinux2-1.0 >&2 && \
     ./update.pl 7.1 10 && \
     echo updating php7.1 node12 amazonlinux2-1.0 >&2 && \
